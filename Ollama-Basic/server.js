273afd475692eba563f46c845920e276c4a85d1a
Ozzy Akben <akbenof@gmail.com>
2024-10-10 23:48:38 -0400

    add click event listener

--- Ollama-Basic/server.js
+++
@@ -1,8 +0,0 @@
-const corsAnywhere = require('cors-anywhere');
-
-corsAnywhere.createServer({
-  originWhitelist: [], // Allow all origins
-}).listen(8080, 'localhost', () => {
-  console.log('CORS Anywhere proxy running on http://localhost:8080');
-});
-
